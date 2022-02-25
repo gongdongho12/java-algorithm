cbcba1cd30af59d8da86609da3d22b21d90b4f2b
Dongho Gang <gongdongho12@gmail.com>
2022-02-25 22:01:53 +0900

    feat: 리트코드 container With Most Water 풀이 추가

---
+++ src/com/dongholab/leetcode/Main.java
@@ -0,0 +1,31 @@
+package com.dongholab.leetcode;
+
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int max = Math.min(height[left], height[right]) * (right - left);
+        while(left < right) {
+            int leftHeight = height[left];
+            int rightHeight = height[right];
+            int temp = Math.min(leftHeight, rightHeight) * (right - left);
+            if (max < temp) {
+                max = temp;
+            }
+
+            if (leftHeight <= rightHeight) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.maxArea(new int[]{ 1,8,6,2,5,4,8,3,7 }) == 49);
+        System.out.println(solution.maxArea(new int[]{ 2,3,4,5,18,17,6 }) == 17);
+    }
+}
